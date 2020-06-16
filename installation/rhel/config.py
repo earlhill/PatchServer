0ecdb3480c936308a393da14eed37ea1a9cfef23
earlhill <57364613+earlhill@users.noreply.github.com>
2020-06-15 22:50:45 -0400

    Update config.py

--- installation/rhel/config.py
+++ installation/rhel/config.py
@@ -1,5 +1,11 @@
 from multiprocessing import cpu_count
+import os
 
 bind = "127.0.0.1:5000"
+
+if os.environ.get("PATCHSERVER_PORT"):
+   print "Change port for patchserver"
+   bind = "127.0.0.1:"+os.environ.get("PATCHSERVER_PORT")
+
 workers = 2
 threads = 2 * cpu_count()
